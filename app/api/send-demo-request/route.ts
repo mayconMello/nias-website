a7e9b8e73bdda13949e86b3ef3f9c20b14496a97
Maycon Ferreira <mayconferr.18@gmail.com>
2026-02-01 21:45:46 -0300

    updated libs

--- app/api/send-demo-request/route.ts
+++ app/api/send-demo-request/route.ts
@@ -1,10 +1,10 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
+
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const { name, email, phone, company } = await request.json();
 
     const { data, error } = await resend.emails.send({
